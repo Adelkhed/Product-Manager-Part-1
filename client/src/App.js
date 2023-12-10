94de838561a74870bb15205a441a5d84e9a8582a
Adel Khedhiri <khedhiri.adel@gmail.com>
2023-12-10 14:47:28 +0100

    somthing wrong in folder client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+
+import CreateProduct from './components/CreateProduct';
+
+function App() {
+  return (
+    
+        <div className='App'>
+           <CreateProduct />
+        </div>
+  );
+}
+
+export default App;
